30e5a1ccac9cc07065149a895898b72189b32ea0
denimendica678 <58472463+denimendica678@users.noreply.github.com>
2021-06-06 12:27:15 +0200

    Add files via upload

---
+++ zavrsniZadatak6/zavrsniZadatak6/KlasaC.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zavrsniZadatak6
+{
+    public class KlasaC
+    {
+        public int Zbroj(int[] brojevi)
+        {
+            int sum = 0;
+
+            foreach (int d in brojevi)
+            {
+                sum += d;
+            }
+
+            return sum;
+
+        }
+        public int Prosjek(int[] brojevi)
+        {
+            int pro = 0;
+
+            foreach (int d in brojevi)
+            {
+                pro += d;
+            }
+
+            pro = pro / brojevi.Length;
+
+            return pro;
+        }
+
+
+    }
+}
---
+++ zavrsniZadatak6/zavrsniZadatak6/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zavrsniZadatak6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            KlasaC prosjek = new KlasaC();
+            
+
+            int[] brojevi = new int[5];
+            Console.WriteLine("Unesite pet brojeva: ");
+
+            for (int i = 0; i < 5; i++)
+            {
+                brojevi[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            Console.Write("Zbroj unesenih brojeva je: " + prosjek.Zbroj(brojevi) + ", a prosjek unesenih brojeva je: " + prosjek.Prosjek(brojevi));
+            
+
+            Console.ReadKey();
+
+        }
+    }
+}
